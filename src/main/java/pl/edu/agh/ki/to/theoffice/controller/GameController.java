614e9bff197ef9f20fb1a0fccd97121437da2160
Marcin Zielonka <305463@student.agh.edu.pl>
2020-12-12 23:13:33 +0100

    added FxWeaver support

--- src/main/java/pl/edu/agh/ki/to/theoffice/controller/GameController.java
+++ src/main/java/pl/edu/agh/ki/to/theoffice/controller/GameController.java
@@ -4,23 +4,28 @@ import javafx.fxml.FXML;
 import javafx.scene.input.KeyEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import pl.edu.agh.ki.to.theoffice.common.observer.Change;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import pl.edu.agh.ki.to.theoffice.components.game.GameControlsComponent;
 import pl.edu.agh.ki.to.theoffice.components.game.GameMapComponent;
 import pl.edu.agh.ki.to.theoffice.domain.game.GameProperties;
 import pl.edu.agh.ki.to.theoffice.domain.game.GameStorage;
-import pl.edu.agh.ki.to.theoffice.domain.game.Game;
-import pl.edu.agh.ki.to.theoffice.domain.game.GameState;
 
 import static pl.edu.agh.ki.to.theoffice.domain.map.Location.Direction;
 
 @Slf4j
-@Controller
-@RequiredArgsConstructor
+@Component
+@FxmlView("/view/game/game.fxml")
 public class GameController {
 
     private final GameStorage gameStorage;
+
+    @Autowired
+    public GameController(GameStorage gameStorage) {
+        this.gameStorage = gameStorage;
+        log.debug("GameController init");
+    }
 
     @FXML
     public GameControlsComponent controls;
